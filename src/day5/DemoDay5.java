e593d3f1f699c2c80e322f4ad420caa4486e84a4
annelisses <140770721+annelisses@users.noreply.github.com>
2023-07-29 10:03:10 -0700

    Update DemoDay5.java
    implemented getData()

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -4,5 +4,8 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		System.out.println("Mostly Sunny 94");
 	}
-
+	//add method
+	public static void getData(){
+		System.out.println("Hey you guys!");
+	}
 }
